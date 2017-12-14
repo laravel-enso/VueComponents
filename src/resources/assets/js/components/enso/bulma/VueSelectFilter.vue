22fcf64fac2c4f58f1b28716ea5204eb1ee01ffd
mauthi <michael.mauthner@netwerker.at>
2017-12-14 10:13:35 +0100

    - new component: vue select filter

---
+++ src/resources/assets/js/components/enso/bulma/VueSelectFilter.vue
@@ -0,0 +1,64 @@
+<template>
+
+    <div class="box vue-filter">
+        <div class="has-text-centered">
+            <b>{{ title }}</b>
+        </div>
+        <vue-select
+            :options=options
+            :value=internalValue
+            @input=update>
+        </vue-select> 
+    </div>
+
+</template>
+
+<script>
+
+import { mapGetters } from 'vuex';
+import VueSelect from '../select/VueSelect.vue';
+
+export default {
+    components: { VueSelect },
+
+    props: {
+        title: {
+            type: String,
+            default: null,
+        },
+        options: {
+            type: Object,
+            default() {
+                return {};
+            },
+        },
+        value: {
+            required: true,
+        },
+    },
+
+    computed: {
+        ...mapGetters('locale', ['__']),
+    },
+
+    data() {
+        return {
+            internalValue: this.value,
+        };
+    },
+
+    methods: {
+        update(value) {
+            this.$emit('input', value);
+        }
+    },
+};
+
+</script>
+
+<style>
+
+    .box.vue-filter {
+        padding: 0.5rem;
+    }
+</style>
