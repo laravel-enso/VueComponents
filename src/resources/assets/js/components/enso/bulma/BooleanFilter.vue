84e59f0585b2f75b335b5c046cc949883fd05148
mauthi <michael.mauthner@netwerker.at>
2018-03-18 16:07:44 +0100

    new component: BooleanFilter

---
+++ src/resources/assets/js/components/enso/bulma/BooleanFilter.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <vue-filter
+            :options="options"
+            v-bind="$attrs"
+            v-on="$listeners">
+    </vue-filter>
+
+</template>
+
+<script>
+
+import VueFilter from '../bulma/VueFilter.vue';
+
+
+export default {
+    components: { VueFilter },
+    props: {
+        yesLabel: {
+            type: String,
+            default: 'Yes',
+        },
+        noLabel: {
+            type: String,
+            default: 'No',
+        },
+    },
+
+    data: function() {
+        return {
+            "options": [{
+                "value": 1,
+                "label": this.yesLabel,
+            }, {
+                "value": 0,
+                "label": this.noLabel,
+            }],
+        }
+    },   
+};
+
+</script>
